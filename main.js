aa9b237f19c91515e5380fab84c99d40d406fcf9
morhaf <125406976+Moonteamprog@users.noreply.github.com>
2023-05-17 12:03:39 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,385 @@
+let menu=document.querySelector('#menu-icon');
+let navbar=document.querySelector('.navbar');
+
+menu.onclick= () => {
+// menu.classList.toggle('bx-x')
+navbar.classList.toggle('open')
+};
+
+
+// prompt("omar")
+
+
+
+var prodcust;
+prodcust=[
+    [
+       "hp2021", 
+       "./images/computer-world-500x500.jpg",
+       "ghfghfghfghfghfgf",
+       "12$",
+       "computer",
+        'show.html?45'
+
+    ],
+    
+    [
+        "hp 2022", 
+        "./images/com12.jpg",
+        "bu urun sizi aiit",
+        "10$",
+        "computer",
+        'show.html?2366'
+
+     ],
+     [
+      "redmi 2021", 
+      "./images/phone1.jpg",
+      "bu urun sizi aiit",
+      "20$",
+      "phone",
+        'show.html'
+    ], 
+     [
+        "redmi 2021", 
+        "./images/phone2.jpg",
+        "bu urun sizi aiit",
+        "20$",
+        "phone",
+        'show.html'
+     ],
+     [
+        "redmi 2021", 
+        "./images/phone2.jpg",
+        "bu urun sizi aiit",
+        "20$",
+        "phone",
+        'show.html'
+     ],
+     [
+        "redmi 2021", 
+        "./images/phone2.jpg",
+        "bu urun sizi aiit",
+        "20$",
+        "phone",
+        'show.html'
+     ],
+     [
+        "redmi 2021", 
+        "./images/phone2.jpg",
+        "bu urun sizi aiit",
+        "20$",
+        "phone",
+        'show.html'
+     ],
+     [
+        "table", 
+        "./images/4تنزيل.jpeg",
+        "bu urun sizi aiit",
+        "20$",
+        "clothes",
+        'show.html'
+     ],
+     [
+        "table", 
+        "./images/4تنزيل.jpeg",
+        "bu urun sizi aiit",
+        "20$",
+        "clothes",
+        'show.html'
+     ],
+     [
+        "table", 
+        "./images/4تنزيل.jpeg",
+        "bu urun sizi aiit",
+        "20$",
+        "clothes",
+        'show.html'
+     ],
+     [
+        "table", 
+        "./images/4تنزيل.jpeg",
+        "bu urun sizi aiit",
+        "20$",
+        "laptop",
+        'show.html'
+     ],
+     [
+        "table", 
+        "./images/4تنزيل.jpeg",
+        "bu urun sizi aiit",
+        "20$",
+        "laptop",
+        'show.html'
+     ],
+     [
+        "table", 
+        "./images/4تنزيل.jpeg",
+        "bu urun sizi aiit",
+        "20$",
+        "laptop",
+        'show.html'
+     ],
+     [
+        "table", 
+        "./images/4تنزيل.jpeg",
+        "bu urun sizi aiit",
+        "20$",
+        "laptop",
+        'show.html'
+     ],
+     [
+        "table", 
+        "./images/4تنزيل.jpeg",
+        "bu urun sizi aiit",
+        "20$",
+        "sports",
+        'show.html'
+     ],
+     [
+      "table", 
+      "./images/4تنزيل.jpeg",
+      "bu urun sizi aiit",
+      "20$",
+      "sports",
+      'show.html'
+   ],
+   [
+      "table", 
+      "./images/4تنزيل.jpeg",
+      "bu urun sizi aiit",
+      "20$",
+      "sports",
+      'show.html'
+   ],
+     [
+        "table", 
+        "./images/4تنزيل.jpeg",
+        "bu urun sizi aiit",
+        "20$",
+        "clothes",
+        'show.html'
+     ],
+     [
+        "table", 
+        "./images/4تنزيل.jpeg",
+        "bu urun sizi aiit",
+        "20$",
+        "clothes",
+        'show.html'
+     ],
+     [
+        "table", 
+        "./images/4تنزيل.jpeg",
+        "bu urun sizi aiit",
+        "20$",
+        "clothes",
+        'show.html'
+     ],
+     [
+      "table", 
+      "./images/4تنزيل.jpeg",
+      "bu urun sizi aiit",
+      "20$",
+      "clothes",
+      'show.html'
+   ],
+   [
+      "table", 
+      "./images/4تنزيل.jpeg",
+      "bu urun sizi aiit",
+      "20$",
+      "clothes",
+      'show.html'
+   ],
+   [
+      "table", 
+      "./images/4تنزيل.jpeg",
+      "bu urun sizi aiit",
+      "20$",
+      "clothes",
+      'show.html'
+   ],
+   [
+      "table", 
+      "./images/4تنزيل.jpeg",
+      "bu urun sizi aiit",
+      "20$",
+      "childern",
+      'show.html'
+   ],
+   [
+      "table", 
+      "./images/4تنزيل.jpeg",
+      "bu urun sizi aiit",
+      "20$",
+      "childern",
+      'show.html'
+   ],
+   [
+      "table", 
+      "./images/4تنزيل.jpeg",
+      "bu urun sizi aiit",
+      "20$",
+      "childern",
+      'show.html'
+   ],
+   [
+      "table", 
+      "./images/4تنزيل.jpeg",
+      "bu urun sizi aiit",
+      "20$",
+      "childern",
+      'show.html'
+   ],
+   [
+      "table", 
+      "./images/4تنزيل.jpeg",
+      "bu urun sizi aiit",
+      "20$",
+      "childern",
+      'show.html'
+   ],
+   [
+      "table", 
+      "./images/4تنزيل.jpeg",
+      "bu urun sizi aiit",
+      "20$",
+      "childern",
+      'show.html'
+   ],
+   [
+      "table", 
+      "./images/4تنزيل.jpeg",
+      "bu urun sizi aiit",
+      "20$",
+      "childern",
+      'show.html'
+   ],
+   [
+      "table", 
+      "./images/4تنزيل.jpeg",
+      "bu urun sizi aiit",
+      "20$",
+      "electronics",
+      'show.html'
+   ],
+   [
+      "table", 
+      "./images/4تنزيل.jpeg",
+      "bu urun sizi aiit",
+      "20$",
+      "electronics",
+      'show.html'
+   ],
+   [
+      "table", 
+      "./images/4تنزيل.jpeg",
+      "bu urun sizi aiit",
+      "20$",
+      "electronics",
+      'show.html'
+   ],
+   [
+      "table", 
+      "./images/4تنزيل.jpeg",
+      "bu urun sizi aiit",
+      "20$",
+      "electronics",
+      'show.html'
+   ],
+   [
+      "table", 
+      "./images/4تنزيل.jpeg",
+      "bu urun sizi aiit",
+      "20$",
+      "electronics",
+      'show.html'
+   ],
+   [
+      "table", 
+      "./images/4تنزيل.jpeg",
+      "bu urun sizi aiit",
+      "20$",
+      "electronics",
+      'show.html'
+   ],
+   [
+      "table", 
+      "./images/4تنزيل.jpeg",
+      "bu urun sizi aiit",
+      "20$",
+      "other",
+      'show.html'
+   ],
+   [
+      "table", 
+      "./images/4تنزيل.jpeg",
+      "bu urun sizi aiit",
+      "20$",
+      "other",
+      'show.html'
+   ],
+   [
+      "table", 
+      "./images/4تنزيل.jpeg",
+      "bu urun sizi aiit",
+      "20$",
+      'show.html'
+      // "<i class='fas fa-heart'></i>"
+   ],
+   
+
+
+]
+
+// user seen on the site
+
+for( var m=0; m<prodcust.length; m++ ){
+    first.innerHTML+=` 
+    
+    <a href="${prodcust[m][5]}"> 
+    <div   class="products ${prodcust[m][4] }"  >
+    <div class="card" style="width:200px"   >
+      <img class="card-img-top" src="${prodcust[m][1] }" >
+      <div class="card-body">
+
+        <p class="card-text">${prodcust[m][2]}</p>
+        <div class="price"   id="price">${prodcust[m][3]}</div>
+        <a href="#" class="btn btn-primary">See more    </a>
+        <div class="kalp"><i class="fas fa-heart"></i></div> 
+    
+    
+    
+    </div>
+  </div>
+  </a>
+    `
+
+
+}
+
+function fun (type ){
+    for(var n=0; n<document.getElementsByClassName ('products').length ; n++ ){
+        document.getElementsByClassName('products')[n].style.display="none"
+    }
+    for(var n=0; n<document.getElementsByClassName (type).length ; n++ ){
+        document.getElementsByClassName(type)[n].style.display="block"
+    }
+}
+
+// products
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
